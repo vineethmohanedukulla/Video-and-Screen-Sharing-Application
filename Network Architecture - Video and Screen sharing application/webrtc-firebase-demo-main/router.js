f0c68ad20dc92ee8111e19422910f075c8a13674
vekfd <78897209+vekfd@users.noreply.github.com>
2021-12-20 21:36:37 -0600

    code commmit
    code commmit

---
+++ Network Architecture - Video and Screen sharing application/webrtc-firebase-demo-main/router.js
@@ -0,0 +1,27 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    404: "404.html",
+    "/": "content.html",
+    "/about": "about.html",
+    "/lorem": "lorem.html",
+};
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+    console.log();
+    console.log(html);
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
